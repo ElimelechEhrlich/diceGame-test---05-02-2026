c730fc14f304ff492843df6b4c02a25e8e131490
Elimelech Ehrlich <ee525566@gmail.com>
2026-02-05 16:48:39 +0200

    The game runs properly.

---
+++ index.js
@@ -0,0 +1,73 @@
+function roll_dice() {
+    const dice1 = Math.floor((Math.random()) * 6) + 1
+    const dice2 = Math.floor((Math.random()) * 6) + 1
+    return [dice1, dice2]
+}
+
+const players = [{ name: 'player1', player_number: '1', score: 0, corrent_turn: false }, { name: 'player2', player_number: '2', score: 0, corrent_turn: false }]
+let index = Math.floor((Math.random()) * 2)
+players[index].corrent_turn = true
+console.log(players[index]);
+
+function turn() {
+
+    console.log(corrent_player);
+
+    const roll_button = document.querySelector('.roll_button')
+    roll_button.addEventListener('click', (e) => {
+        e.preventDefault()
+        const corrent_player = players.find((player) => player.corrent_turn)
+        const corrent_score = document.querySelector(`.corrent_score${corrent_player.player_number}`)
+        console.log(corrent_player);
+        if (corrent_player.corrent_turn) {
+            if (corrent_player.score < 100) {
+                const dices_results = roll_dice()
+                if (dices_results[0] !== dices_results[1]) {
+                    const sum = dices_results[0] + dices_results[1]
+                    console.log(sum);
+                    console.log(corrent_score);
+                    corrent_score.textContent = Number(corrent_score.textContent) + sum
+                    // corrent_player.score += Number(corrent_score.textContent)
+                } else {
+                    corrent_score.textContent = 0
+                    switch_player(corrent_player)
+                }
+            }
+        }
+    })
+    const hold_button = document.querySelector('.hold_button')
+    hold_button.addEventListener('click', (e) => {
+        e.preventDefault()
+        const corrent_player = players.find((player) => player.corrent_turn)
+        const corrent_score = document.querySelector(`.corrent_score${corrent_player.player_number}`)
+        corrent_player.score += Number(corrent_score.textContent)
+        console.log(corrent_player);
+        const sum_score = document.querySelector(`.sum_score${corrent_player.player_number}`)
+        sum_score.textContent = Number(sum_score.textContent) + Number(corrent_score.textContent)
+        corrent_score.textContent = '0'
+        console.log(corrent_score);
+        switch_player(corrent_player)
+        console.log(corrent_score);
+
+        // turn(corrent_player)
+
+    })
+}
+
+
+
+
+
+
+function switch_player(corrent_player) {
+    console.log(corrent_player);
+    corrent_player.corrent_turn = false
+    corrent_player === players[0] ? corrent_player = players[1] : corrent_player = players[0]
+    corrent_player.corrent_turn = true
+    console.log(corrent_player);
+}
+
+
+let corrent_player = players.find((player) => player.corrent_turn)
+console.log(corrent_player);
+turn()
